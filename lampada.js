b636a95f96edbd2fa3b54f232aac8e1ea460e9f6
LariVillas <107321916+LariVillas@users.noreply.github.com>
2022-11-20 03:29:26 -0400

    Add files via upload

---
+++ lampada.js
@@ -0,0 +1,7 @@
+const botao = document.getElementById('botao');
+
+function mensagem (){
+    alert('Vc n consegue mensurar o quanto me arrependo!! Vc é meu universo puro!! Eu sinto muitíssimo todos os dias por ter te machucado!! Vc é oq mais me importa no mundo!! Me desculpa por não ter te amado direito. Eu prometo q nunca mais vo me transformar naquela garota, eu sou a villas boas da tua colvara, a larissa da tua júlia!! Eu amo vc mais q td');
+}
+
+botao.addEventListener('click', mensagem);
